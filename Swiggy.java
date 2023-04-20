7a772655253af3279389f1c160cc45fa68ea13fd
SANTHOSH-NV <santhoshkumarnv.xworkz@gmail.com>
2023-04-20 12:12:45 +0530

    adding some java files

---
+++ Swiggy.java
@@ -0,0 +1,92 @@
+class Swiggy
+{
+public static double searchItem(String itemName)
+{
+System.out.println("searchItem started");
+if("Dum Biryani" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 150.00;
+}
+if("set dose" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 70.00;
+}
+if("egg rice" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 40.00;
+}
+if("fried rice" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 65.00;
+}
+if("chilli chicken" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 100.00;
+}
+if("pav baji" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 100.00;
+}
+if("samosa" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 20.00;
+}
+if("carrot halwa" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 70.00;
+}
+if("egg curry" == itemName)
+{
+System.out.println(" thanks for searching " + itemName);
+return 60.00;
+}
+if("chicken palav" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return  100.00;
+}
+if("veg palav" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 60.00;
+}
+if("chicken curry" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return 150.00;
+}
+if("potato twister" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return  60.00;
+}
+if("vada pav" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return  30.00;
+}
+if("maggie" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return  45.00;
+}
+if("noodles" == itemName)
+{
+System.out.println("thanks for searching" +itemName);
+return  60.00;
+}
+{
+System.out.println("searchItem ended");
+return 0.00;
+}
+}
+}
+
