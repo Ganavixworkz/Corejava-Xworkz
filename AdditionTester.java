08b00e7a48729bcfde5d930090ff1f73c00a621a
Ganavixworkz <ganavinv.xworkz@gmail.com>
2023-04-27 09:49:51 +0530

    files

---
+++ AdditionTester.java
@@ -0,0 +1,14 @@
+class AdditionTester
+{
+public static void main(String add[])
+   {
+  System.out.println("main started");
+  System.out.println("addition of two numbers");
+  System.out.println("addition of three numbers");
+    int sumOfTwoNumbers=Addition2.add(45,50);
+    int sumOfThreeNumbers=Addition2.add(90,40,20);
+  System.out.println("addition of two numbers:"+sumOfTwoNumbers);
+  System.out.println("addition of three numbers:"+sumOfThreeNumbers);
+  System.out.println("main ended");
+   }
+}
