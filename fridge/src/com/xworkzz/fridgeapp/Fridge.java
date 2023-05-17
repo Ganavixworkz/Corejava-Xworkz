ab8988e1800598259e32ef7b38f8f810f7823c9f
Ganavixworkz <ganavinv.xworkz@gmail.com>
2023-05-17 20:53:20 +0530

    files

---
+++ fridge/src/com/xworkzz/fridgeapp/Fridge.java
@@ -0,0 +1,13 @@
+package com.xworkzz.fridgeapp;
+
+public class Fridge {
+
+
+        static String company= "Haier";
+        static int price= 10990;
+        public static void main(String a[])
+        {
+            System.out.println("The Fridge company is "+ "Haier");
+            System.out.println("The Fridge price is Rs."+ price+ "  limited offer");
+        }
+}
