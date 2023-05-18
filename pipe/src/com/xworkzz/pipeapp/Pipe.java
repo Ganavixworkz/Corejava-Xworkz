a57095dfe7d68da7f662b332e32541cf12d91af3
Ganavixworkz <ganavinv.xworkz@gmail.com>
2023-05-18 12:27:32 +0530

    files

---
+++ pipe/src/com/xworkzz/pipeapp/Pipe.java
@@ -0,0 +1,16 @@
+package com.xworkzz.pipeapp;
+
+public class Pipe {
+
+        String color;
+        String type;
+        double price;
+        int length;
+        int width;
+        public void connect(){
+            System.out.println("To convey substances which can flow");
+
+        }
+
+
+    }
